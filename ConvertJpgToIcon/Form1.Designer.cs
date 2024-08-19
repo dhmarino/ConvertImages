1a8ad01392565cadd5af6d5ebfd3201f6893a765
dhmarino <diego.marino@valid.com>
2024-08-19 11:00:55 -0300

    Commit inicial

---
+++ ConvertJpgToIcon/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ConvertJpgToIcon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.buttonConvert2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonConvert.Location = new System.Drawing.Point(283, 84);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(202, 97);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "JPG to ICO";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // buttonConvert2
+            // 
+            this.buttonConvert2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonConvert2.Location = new System.Drawing.Point(283, 248);
+            this.buttonConvert2.Name = "buttonConvert2";
+            this.buttonConvert2.Size = new System.Drawing.Size(202, 97);
+            this.buttonConvert2.TabIndex = 1;
+            this.buttonConvert2.Text = "PNG to ICO";
+            this.buttonConvert2.UseVisualStyleBackColor = true;
+            this.buttonConvert2.Click += new System.EventHandler(this.buttonConvert2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonConvert2);
+            this.Controls.Add(this.buttonConvert);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Conversor Imagenes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Button buttonConvert2;
+    }
+}
+
---
+++ ConvertJpgToIcon/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ConvertJpgToIcon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            // Crea y configurar el OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "JPG Files (*.jpg)|*.jpg",
+                Title = "Seleccionar un archivo JPG"
+            };
+
+            // Mostrar el diálogo para seleccionar el archivo
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta del archivo seleccionado
+                string inputPath = openFileDialog.FileName;
+
+                // Definir la ruta de salida para el archivo .ico
+                string outputPath = Path.ChangeExtension(inputPath, ".ico");
+
+                // Convertir JPG a ICO
+                ConvertJpgToIcon(inputPath, outputPath);
+
+                // Mostrar un mensaje al usuario
+                MessageBox.Show($"Archivo ICO guardado en: {outputPath}", "Conversión exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void ConvertJpgToIcon(string inputPath, string outputPath)
+        {
+            using (Bitmap bitmap = new Bitmap(inputPath))
+            {
+                // Crear un nuevo archivo ICO
+                using (FileStream fs = new FileStream(outputPath, FileMode.Create))
+                {
+                    // Escribir el header del icono
+                    fs.WriteByte(0); fs.WriteByte(0); // Reserved
+                    fs.WriteByte(1); fs.WriteByte(0); // Type (1 = ICON)
+                    fs.WriteByte(1); fs.WriteByte(0); // Image count
+
+                    // Escribir la entrada de directorio para el icono
+                    fs.WriteByte(32); // Width
+                    fs.WriteByte(32); // Height
+                    fs.WriteByte(0); // Color count (0 = no palette)
+                    fs.WriteByte(0); // Reserved
+                    fs.WriteByte(1); fs.WriteByte(0); // Planes
+                    fs.WriteByte(32); fs.WriteByte(0); // Bit count
+                    byte[] bitmapData;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        Bitmap iconBitmap = new Bitmap(bitmap, new Size(32, 32));
+                        iconBitmap.Save(ms, ImageFormat.Png);
+                        bitmapData = ms.ToArray();
+                    }
+                    fs.Write(BitConverter.GetBytes(bitmapData.Length), 0, 4); // Size of image data
+                    fs.Write(BitConverter.GetBytes(22), 0, 4); // Offset to image data
+
+                    // Escribir los datos de la imagen
+                    fs.Write(bitmapData, 0, bitmapData.Length);
+                }
+            }
+        }
+
+        private void buttonConvert2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "PNG Files (*.png)|*.png",
+                Title = "Seleccionar un archivo PNG"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string inputPath = openFileDialog.FileName;
+                string outputPath = Path.ChangeExtension(inputPath, ".ico");
+
+                ConvertPngToIcon(inputPath, outputPath);
+
+                MessageBox.Show($"Archivo ICO guardado en: {outputPath}", "Conversión exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void ConvertPngToIcon(string inputPath, string outputPath)
+        {
+            using (Bitmap bitmap = new Bitmap(inputPath))
+            {
+                // Crear un nuevo archivo ICO
+                using (FileStream fs = new FileStream(outputPath, FileMode.Create))
+                {
+                    // Escribir el header del icono
+                    fs.WriteByte(0); fs.WriteByte(0); // Reserved
+                    fs.WriteByte(1); fs.WriteByte(0); // Type (1 = ICON)
+                    fs.WriteByte(1); fs.WriteByte(0); // Image count
+
+                    // Escribir la entrada de directorio para el icono
+                    fs.WriteByte(32); // Width
+                    fs.WriteByte(32); // Height
+                    fs.WriteByte(0); // Color count (0 = no palette)
+                    fs.WriteByte(0); // Reserved
+                    fs.WriteByte(1); fs.WriteByte(0); // Planes
+                    fs.WriteByte(32); fs.WriteByte(0); // Bit count
+                    byte[] bitmapData;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        Bitmap iconBitmap = new Bitmap(bitmap, new Size(32, 32));
+                        iconBitmap.Save(ms, ImageFormat.Png);
+                        bitmapData = ms.ToArray();
+                    }
+                    fs.Write(BitConverter.GetBytes(bitmapData.Length), 0, 4); // Size of image data
+                    fs.Write(BitConverter.GetBytes(22), 0, 4); // Offset to image data
+
+                    // Escribir los datos de la imagen
+                    fs.Write(bitmapData, 0, bitmapData.Length);
+                }
+            }
+        }
+    }
+}
